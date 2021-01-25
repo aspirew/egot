6777be8dd0136c114ffe1841e8ab02ab81d45d4b
Pralkopodobny <marceli.sokolski@onet.pl>
2021-01-25 20:03:39 +0000

    Add new export

--- src/routes.test.ts
+++ src/routes.test.ts
@@ -2,7 +2,7 @@ import { keys } from 'ts-transformer-keys';
 import { teren } from "./interfaces"
 import app from "./app"
 import { mockupArea } from './mockupData'
-import { poolObject, pool } from './dbconn'
+import { poolObject, pool, testPool } from './dbconn'
 import pointsService from './services/pointsService';
 
 
@@ -13,11 +13,21 @@ poolObject.setTesting()
 
 const del = async (name: String) => {
   try{
-    const result = await pool.promise().query('DELETE FROM punkt WHERE nazwa = ?', name);
+    const result = await testPool.promise().query('DELETE FROM Punkt WHERE nazwa = ?', name);
     console.log(result)
   }
     catch(err){
       console.log(err)
+    }
+}
+
+const delSeg = async (name: String) => {
+  try{
+    const result = await pool.promise().query('DELETE FROM Odcinek WHERE nazwa = ?', name);
+    console.log(result)
+  }
+    catch(err){
+      //console.log(err)
     }
 }
 
@@ -27,8 +37,19 @@ const add = async (id: Number, name: String, Pracownik_PTTK: Number, npm: Number
       console.log(result)
     }
     catch(err){
-      console.log(err)
+      //console.log(err)
     }
+}
+
+const addSeg = async (id: Number, name: String, pp : Number, pk: Number, ter: Number, dl: Number, punkt : Number, punkt2: Number) => {
+  try{
+    const result = await pool.promise().query('INSERT INTO Odcinek VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [id, name, pp, pk, ter, dl, punkt, punkt2]);
+    console.log(result)
+  }
+  catch(err){
+    console.log('Add Segment ERROR')
+    console.log(err)
+  }
 }
 
 
@@ -59,6 +80,7 @@ describe('Post Endpoints', () => {
       await del(name)
       
       expect(res.body.success).toEqual(true)
+      expect(res.body.message).toEqual("New point succesfully add")
 
   })
 
@@ -99,6 +121,116 @@ describe('Post Endpoints', () => {
           expect(res.body.success).toEqual(false)
     
       })
+
+      //
+      it('Posting new segment should succeed', async () => {
+        const name = 'Wyszeborska Trasa Dla Miłości Serca'
+        const start = 47
+        const end = 46
+        const dlugosc = 100
+        const teren = 3
+        const punktacja = 10
+        const punktacja2 = 10
+
+        const res = await request(app)
+        .post('/api/segment/add').send({
+            newSegment: {
+            ID: null,
+            Nazwa: name,
+            PunktPoczatkowy: start,
+            PunktKoncowy: end,
+            Teren: teren,
+            Dlugosc: dlugosc,
+            Punktacja: punktacja,
+            PunktacjaOdKonca: punktacja2
+          }
+        })
+
+        await delSeg(name)
+
+        expect(res.body.success).toEqual(true)
+        expect(res.body.message).toEqual("New segment succesfully add")
+      })
+
+      it('Posting new segment with existing name should fail', async () => {
+        const name = 'AB'
+        const start = 47
+        const end = 46
+        const dlugosc = 100
+        const teren = 3
+        const punktacja = 10
+        const punktacja2 = 10
+
+        const res = await request(app)
+        .post('/api/segment/add').send({
+            newSegment: {
+            ID: null,
+            Nazwa: name,
+            PunktPoczatkowy: start,
+            PunktKoncowy: end,
+            Teren: teren,
+            Dlugosc: dlugosc,
+            Punktacja: punktacja,
+            PunktacjaOdKonca: punktacja2
+          }
+        })
+
+        expect(res.body.success).toEqual(false)
+      })
+
+      it('posting new segment with non positive dlugosc should fail', async () =>{
+        const name = 'Wyszeborska Trasa Dla Miłości Serca'
+        const start = 47
+        const end = 46
+        const dlugosc = -100
+        const teren = 3
+        const punktacja = 10
+        const punktacja2 = 10
+
+        const res = await request(app)
+        .post('/api/segment/add').send({
+            newSegment: {
+            ID: null,
+            Nazwa: name,
+            PunktPoczatkowy: start,
+            PunktKoncowy: end,
+            Teren: teren,
+            Dlugosc: dlugosc,
+            Punktacja: punktacja,
+            PunktacjaOdKonca: punktacja2
+          }
+        })
+      })
+
+      it('deleting segment which is not used in any przejscie should succeed', async () => {
+          const id = 38
+          const name = 'Moszna - Rozdroże pod Osówką'
+          const pp = 24
+          const pk = 25
+          const teren = 1
+          const dl = 200
+          const punkt = 1
+          const punkt2 = 1
+    
+          const res = await request(app)
+            .delete(`/api/segment/${id}`)
+
+          await addSeg(id, name, pp, pk, teren, dl, punkt, punkt2)
+          console.log(res.body)
+          expect(res.body.success).toEqual(true)
+
+      })
+
+      it('deleting segment which is used in any przejscie should not succeed', async () => {
+        const id = 1
+  
+        const res = await request(app)
+          .delete(`/api/segment/${id}`)
+
+        console.log(res.body)
+        expect(res.body.success).toEqual(false)
+
+    })
 
     
 })
